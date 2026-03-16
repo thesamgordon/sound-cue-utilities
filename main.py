78b9ecc03325f84fc87358d77afb19aaa91910a5
Sam Gordon <sam@thesamgordon.com>
2026-03-16 11:24:46 -0400

    feat: use .env file

--- main.py
+++ main.py
@@ -2,91 +2,35 @@ from file_handler import read_excel_data
 from snippet_generator import generate_snippets
 from qlab_network_generator import generate_network_cues
 import os
-
-def locate_xlsx_files():
-    return [file for file in os.listdir(".") if file.endswith(".xlsx")]
-
-def get_user_input(prompt, default, validation_func=None):
-    while True:
-        user_input = input(prompt)
-        if validation_func is None or validation_func(user_input) == True:
-            return user_input
-        elif user_input == "":
-            return default
-        else:
-            print("Invalid input. Please try again.")
-
-def validate_yes_no(input_str):
-    if input_str.lower() in ["y", "yes"]:
-        return True
-    elif input_str.lower() in ["n", "no"]:
-        return True
-    else:
-        print("Please enter Y or N.")
-        return None 
-    
-def validate_int_input(input_str):
-  try:
-      int(input_str)
-      return True
-  except ValueError:
-      return None
+from dotenv import load_dotenv
+load_dotenv()
 
 def main():
-    xlsx_files = locate_xlsx_files()
+    xlsx_file = os.getenv("FILE_NAME")
 
-    if not xlsx_files:
-        print("No xlsx files found in the current directory.")
-        return
-
-    if len(xlsx_files) > 1:
-        for i, xlsx_file in enumerate(xlsx_files):
-            print(f"{i + 1}. {xlsx_file}")
-
-        file_index = int(get_user_input("Choose a file to generate snippets from (1): ", "1", validate_int_input)) -1
-
-        if 0 <= file_index < len(xlsx_files):
-            xlsx_file = xlsx_files[file_index]
-        else:
-            print("Invalid file selection.")
-            return
-    else:
-        xlsx_file = xlsx_files[0]
-
-    skip_rows = int(get_user_input("Start at row (1): ", "1", validate_int_input)) - 1
+    skip_rows = int(os.getenv("ROW_START", "0")) - 1
     if skip_rows < 0:
         skip_rows = 0
 
-    identifying_character = get_user_input("Identifying character (X): ", "X")
-    if not identifying_character:
-        identifying_character = "X"
+    identifying_character = os.getenv("IDENTIFYING_CHARACTER", "X")
 
     print(f"Choose cue generation method:")
     print("1. Generate X32 Snippets")
     print("2. Generate QLab Cues")
-    method = get_user_input("Method (1): ", "1", validate_int_input)
+    method = os.getenv("METHOD", "SNIPPET")
 
-    use_dca = False
-    dca_identifier = None
+    use_dca = os.getenv("USE_DCA", "false").lower() in ["true", "1", "yes"]
+    dca_identifier = os.getenv("DCA_IDENTIFIER", "/") if use_dca else None
 
     show_file_name = xlsx_file.replace(".xlsx", "")
     data_frame = read_excel_data(xlsx_file, skip_rows)
-
-    if get_user_input(f"Would you like to use DCAs for ensamble? (Cast on 1, Ensemble on 2) (Y/N): ", "N", validate_yes_no) in ["Y", "y", "Yes", "yes"]:
-        use_dca = True
-
-        dca_identifier = get_user_input("DCA Identifier (-): ", "-")
-    else:
-        use_dca = False
         
-    if method == "1":
-
-            
+    if method == "SNIPPET":
         generate_snippets(data_frame, show_file_name, "output", identifying_character, use_dca, dca_identifier)
+    elif method == "NETWORK":
+        generate_network_cues(data_frame, identifying_character, dca_identifier, "1")
     else:
-        network_patch = get_user_input("Network Patch (1): ", "1", validate_int_input)
-        generate_network_cues(data_frame, identifying_character, dca_identifier, network_patch)
-
+        print("Invalid method specified in .env file. Please choose either 'SNIPPET' or 'NETWORK'.")
 
 if __name__ == "__main__":
     main()
